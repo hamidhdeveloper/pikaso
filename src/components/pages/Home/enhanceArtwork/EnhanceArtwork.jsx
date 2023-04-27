13f5d3de4a4c1a57df8ddcd52228ab5e59d20bcf
Hamid Hussain <hamidhussain740@gmail.com>
2023-04-27 06:23:49 -0700

    home page artwork responsive done

--- src/components/pages/Home/enhanceArtwork/EnhanceArtwork.jsx
+++ src/components/pages/Home/enhanceArtwork/EnhanceArtwork.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import React from "react";
 import styles from "./EnhanceArtwork.module.css";
-import rectangle1 from '../../../../assets/images/rectangle-30760@2x.png'
-import rectangle2 from '../../../../assets/images/rectangle-30772@2x.png'
-import rectangle5 from '../../../../assets/images/rectangle-5.svg'
+import rectangle1 from "../../../../assets/images/rectangle-30760@2x.png";
+import rectangle2 from "../../../../assets/images/rectangle-30772@2x.png";
 const EnhanceArtwork = () => {
   return (
     <>
       <div className={styles.enhanceartwork}>
-      <div className={styles.enhanceArtwork}>
-        <b className={styles.creating}>
-          <span>Artwo</span>
-          <span className={styles.rk}>rk</span>
-        </b>
-        <b className={styles.start}>{`Enhance `}</b>
-        <img
-          className={styles.enhanceArtworkChild}
-          alt="retangular"
-          src={rectangle1}
-        />
-        <img
-          className={styles.enhanceArtworkItem}
-          alt="reactangular 2"
-          src={rectangle2}
-        />
-        <div className={styles.vectorParent}>
-          <img className={styles.groupChild} alt="" src={rectangle5} />
-          <div className={styles.generate}>{`Generate `}</div>
+        <div className={styles.enhanceArtwork}>
+          <div className={styles.enhanceArtworkTop}>
+            <b className={styles.start}>{`Enhance `}</b>
+            <b className={styles.creating}>
+              <span>Artwo</span>
+              <span className={styles.rk}>rk</span>
+            </b>
+          </div>
+          <div className={styles.enhanceArtworkButtom}>
+            <div className={styles.enhanceArtworkButtomLeft}>
+              <img
+                className={styles.enhanceArtworkItem}
+                alt="reactangular 2"
+                src={rectangle2}
+              />
+            </div>
+            <div className={styles.enhanceArtworkButtomRight}>
+              <img
+                className={styles.enhanceArtworkChild}
+                alt="retangular"
+                src={rectangle1}
+              />
+            </div>
+          </div>
+
+          <div className={styles.vectorParent}>
+            <button className={styles.startNowbtn}>{`Generate `}</button>
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default EnhanceArtwork
+export default EnhanceArtwork;
