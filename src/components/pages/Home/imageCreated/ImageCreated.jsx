5d913aabba92a70bb26093c44000c7920f96834f
Hamid Hussain <hamidhussain740@gmail.com>
2023-04-27 08:06:24 -0700

    home page artwork responsive done 1 section pending

--- src/components/pages/Home/imageCreated/ImageCreated.jsx
+++ src/components/pages/Home/imageCreated/ImageCreated.jsx
@@ -7,43 +7,62 @@ import rectangle4 from '../../../../assets/images/rectangle-30764@2x.png'
 import rectangle5 from '../../../../assets/images/rectangle-30763@2x.png'
 import rectangle6 from '../../../../assets/images/rectangle-30765@2x.png'
 
+
 const ImageCreated = () => {
   return (
     <>
-    <div className={styles.imagecreated}>
-      <div className={styles.imagecreatedChild} />
-      <b className={styles.imagesCreated}>Images Created</b>
-      <div className={styles.imageCreated}>
-        <img
-          className={styles.imageCreatedChild}
-          alt="rectangle"
-          src={rectangle1}
-        />
+      <div className={styles.imagecreated}>
+      <div className={styles.imagecreatedChildContainer} />
+      <div className={styles.imagecreatedChildTop}>
+       <b className={styles.imagesCreated}>Images Created</b>
+      </div>
+      <div className={styles.imagecreatedChildButtom}>
+        <div className={styles.imagesAll}>
+        <div className={styles.imagesAllCol1}>
         <img
           className={styles.imageCreatedItem}
           alt=""
           src={rectangle2}
         />
+        
+        </div>
+        <div className={styles.imagesAllzCol2}>
         <img
-          className={styles.imageCreatedInner}
-          alt=""
-          src={rectangle3}
+          className={styles.imageCreatedChildItem2}
+          alt="rectangle"
+          src={rectangle1}
         />
         <img
-          className={styles.rectangleIcon}
+          className={styles.rectangleIcon4}
           alt=""
           src={rectangle4}
         />
         <img
-          className={styles.imageCreatedChild1}
+          className={styles.imageCreatedInner5}
+          alt=""
+          src={rectangle3}
+        />
+        <img
+          className={styles.imageCreatedChild11}
           alt=""
           src={rectangle5}
         />
         <img
-          className={styles.imageCreatedChild2}
+          className={styles.imageCreatedChild22}
           alt=""
           src={rectangle6}
-        />
+        /> 
+        </div>
+        
+        
+        
+         
+        
+        </div>
+      </div>
+      
+      <div className={styles.imageCreated}>
+        
       </div>
     </div>
       
@@ -52,3 +71,4 @@ const ImageCreated = () => {
 }
 
 export default ImageCreated
+
