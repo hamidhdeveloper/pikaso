8f8bfc8076300c1f08c683b2a5aeb660b18bbf6f
Hamid Hussain <hamidhussain740@gmail.com>
2023-05-10 14:47:25 +0300

    toast done

--- src/components/pages/toast/Toast.jsx
+++ src/components/pages/toast/Toast.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const Toast = ({ type, message }) => {
-  const toastOptions = {
-    position: 'bottom-right',
+export const errorToast = (message) => {
+  toast.error(message, {
+    position: "bottom-right",
     autoClose: 4000,
     hideProgressBar: true,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
     progress: undefined,
-    style: {
-      color: type === 'error' ? 'black' : '#DFA968',
-    },
-  };
+    style:{color:'black'}
+  });
+}
 
-  return <ToastContainer {...toastOptions}>{toast[type](message)}</ToastContainer>;
-};
-
-Toast.propTypes = {
-  type: PropTypes.oneOf(['success', 'error']).isRequired,
-  message: PropTypes.string.isRequired,
-};
-
-export default Toast;
+export const successToast = (message) => {
+  toast.success(message, {
+    position: "bottom-right",
+    autoClose: 4000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    style:{color:'#DFA968'}
+  });
+}
